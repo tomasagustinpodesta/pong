50dd947b8624a505fc3e76c04a3312b03f162a49
Tomas Podesta <tomasagustinpodesta@gmail.com>
2022-10-06 11:57:59 +0200

    Fonts, paddles, ball and background
    I added the four elements into my project, I found this problem where I have this two black bars on the sides, but I couldnt found the issue yet. For now I am saving this progress.

--- Functions_pong.lua
+++ Functions_pong.lua
@@ -15,6 +15,12 @@ function love.load()
     -- This function prevents the text to be blurry and also to set it a little more 8bit like
     love.graphics.setDefaultFilter('nearest', 'nearest')
     
+    -- Putting the file I downloaded to set the font that Im going to use
+    font_pong = love.graphics.newFont('font.ttf', 8)
+
+    -- setting the font I downloaded 
+    love.graphics.setFont(font_pong)
+
     -- This code sets the screen that we are going to use
     push: setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -33,6 +39,8 @@ end
 function love.draw()
     -- Now we are using the virtual resolution with push:apply
     push:apply("start")
+    
+    love.graphics.clear(40/255, 45/255, 52/255, 1)
 
     -- This one lets me print, is incredible to look for the values of the variables.
     -- Still dont know a lot about it, but for now it works for me
@@ -40,10 +48,18 @@ function love.draw()
     love.graphics.printf(
         "Hello Pong",
         0,
-        VIRTUAL_HEIGHT / 2 - 6,
+        VIRTUAL_HEIGHT / 5, -- I changed the distance so the text doesnt collide with the ball
         VIRTUAL_WIDTH,
         "center"
     )
+
+    love.graphics.rectangle("fill", 10, 30, 5, 20)
+
+    love.graphics.rectangle("fill", VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+
+    love.graphics.rectangle("fill", VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
+
     -- Stop using the virtual resolution
     push:apply("end")
 end
